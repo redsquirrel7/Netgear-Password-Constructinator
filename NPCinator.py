b66195ba2606c9babcf967d37d2d04183e0d7ae0
Brad Nelson <34318895+redsquirrel7@users.noreply.github.com>
2018-02-16 11:17:19 -0800

    Add files via upload

--- NPCinator.py
+++ NPCinator.py
@@ -9,23 +9,78 @@
 #   _____             _               _   _         _
 #  |     |___ ___ ___| |_ ___ _ _ ___| |_|_|___ ___| |_ ___ ___
 #  |   --| . |   |_ -|  _|  _| | |  _|  _| |   | .'|  _| . |  _|
-#  |_____|___|_|_|___|_| |_| |___|___|_| |_|_|_|__,|_| |___|_|   v0.2.0
+#  |_____|___|_|_|___|_| |_| |___|___|_| |_|_|_|__,|_| |___|_|   v0.1.0
 #                                                                      
 # Generates default wifi passwords for Netgear routers
 #
 # By: Brad Nelson (Squirrel)
 # Twitter: @redsquirrel_7
 #
-# Adjective and Noun files downloaded from: http://www.ashley-bovan.co.uk/words/partsofspeech.html
-#
 
+import requests
+from bs4 import BeautifulSoup
+import time
 
 adjectives = []
 nouns = []
 numbers = []
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] 
 
 
-# Get the words from adjectives.txt and nouns.txt
+# Crawl Merriam-Webster.com and create a list of 
+# all nouns and adjectives in the english language
+# 
+# THIS FUNCTION IS NOT WORKING!!!!
+# Pretty sure Merriam-Webster doesn't like me crawling their site
+# They seem to be denying the connection after a while and the program crashes
+def dictionary_crawler(max_pages):
+    global adjectives
+    global nouns
+    global alphabet
+    l = 0
+    print("Hold tight ladies and gents! \nJust getting a list of words...")
+    while l < len(alphabet):
+        page = 1
+        letter = input("Input Letter: ") #alphabet[l]
+        #print(str(letter))
+        url = "https://www.merriam-webster.com/browse/dictionary/" + str(letter)
+        source_code = requests.get(url)
+        plain_text = source_code.text
+        soup = BeautifulSoup(plain_text, "html.parser")
+        for span in soup.findAll('span', {'class': 'counters'}):
+            page_numbers = span.string
+            page_numbers = page_numbers.split(' ')
+            max_pages = page_numbers[3]
+        while page <= int(max_pages):
+            url = "https://www.merriam-webster.com/browse/dictionary/" + str(letter) + "/" + str(page)
+            source_code = requests.get(url)
+            plain_text = source_code.text
+            soup = BeautifulSoup(plain_text, "html.parser")
+            for link in soup.findAll('a'):
+                url = link.get('href')
+                word = link.string
+                if str(url) == "/dictionary/" + str(word):
+                    page_crawler(url, word)
+                    #time.sleep(5)
+            page += 1
+        l += 1
+    adjectives = [a.split(' ')[0] for a in adjectives]
+    adjectives = [a.split('-')[0] for a in adjectives]
+    nouns = [n.split(' ')[0] for n in nouns]
+    nouns = [n.split('-')[0] for n in nouns]
+    adjectives = set(adjectives)
+    adjectives = list(adjectives)
+    nouns = set(nouns)
+    nouns = list(nouns)
+    f = open("adjectives.txt", "w+")
+    for a in adjectives:
+        f.write(a + "\n")
+    f.close()
+    f = open("nouns.txt", "w+")
+    for n in nouns:
+        f.write(n + "\n")
+    f.close()
+
 def get_words():
     global adjectives
     global nouns
@@ -38,6 +93,23 @@ def get_words():
     n = f.read()
     nouns = n.split('\n')
     f.close()
+
+
+
+# Crawl webpage of word and determine if it's a 
+# noun or adjective. If so, add it to the corresponding list.
+def page_crawler(url, word):
+    page_url = "https://www.merriam-webster.com" + str(url)
+    page_source = requests.get(page_url)
+    page_text = page_source.text
+    page_soup = BeautifulSoup(page_text, "html.parser")
+    print("Crawling page: %s" % page_url)
+    for link in page_soup.findAll('span', {'class': 'fl'}):
+        word_type = link.text
+        if str(word_type) == "adjective": 
+            adjectives.append(word)
+        elif str(word_type) == "noun":
+            nouns.append(word)
 
 # Generate a list of numbers from 000 to 999
 def number_gen():
@@ -55,19 +127,15 @@ def number_gen():
             number += 1
 
 # Smoosh all of it together into juicy default passwords!
-# Uncomment the lines in this function to create a password file
-# I do not recommend you do this however...
-# The file will be freaking massive!
 def smoosh():
     global adjectives
     global nouns
     global numbers
-    #f = open("default-netgear-passwords.txt", "w+")  
+    #f = open("default-netgear-passwords.txt", "w+")
     for a in adjectives:
         for n in nouns:
             for i in numbers:
                 password = str(a) + str(n) + str(i)
-                password = password.lower()
                 print(password)
                 #f.write(password + "\n")
     #f.close()
